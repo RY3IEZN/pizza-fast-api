dbb469812c06c96944603e71d26626aac2ef8bbe
unknown <unekue2@gmail.com>
2024-02-11 10:56:19 +0100

    created apis, setupdb, manually create db tables

--- db/db_setup.py
+++ db/db_setup.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://uneku:pgadmin@18.171.190.230:5432/pizzadb"
+SQLALCHEMY_DATABASE_URL = ""
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={}, future=True)
 
